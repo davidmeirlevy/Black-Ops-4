a120061825eb234b4b0d2e87ce8b4f596b2d73ab
MuliTu <muli.turiel@gmail.com>
2018-10-29 15:32:44 +0200

    Updates

--- precache-manifest.61d1ef6427af1c25d6bbc8c77b75f2eb.js
+++ precache-manifest.61d1ef6427af1c25d6bbc8c77b75f2eb.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Black-Ops-4/static/js/runtime~main.c55d587a.js"
   },
   {
-    "revision": "2da6bdfbd3af0774e76d",
-    "url": "/Black-Ops-4/static/js/main.2da6bdfb.chunk.js"
+    "revision": "9c50bbab054a2711366d",
+    "url": "/Black-Ops-4/static/js/main.9c50bbab.chunk.js"
   },
   {
     "revision": "b2f253141796e55386a6",
     "url": "/Black-Ops-4/static/js/1.b2f25314.chunk.js"
   },
   {
-    "revision": "2da6bdfbd3af0774e76d",
-    "url": "/Black-Ops-4/static/css/main.0d60bbaa.chunk.css"
+    "revision": "9c50bbab054a2711366d",
+    "url": "/Black-Ops-4/static/css/main.fdca5f22.chunk.css"
   },
   {
-    "revision": "03a9b5b7e18a1b95ff241db8c2b0acdd",
+    "revision": "88f4c5639de2ffc68ed0f973f1d15531",
     "url": "/Black-Ops-4/index.html"
   }
 ];

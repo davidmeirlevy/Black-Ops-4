7a803ba88b260219ace7e8e1b54b77588f248fb6
MuliTu <muli.turiel@gmail.com>
2018-10-30 22:48:17 +0200

    added prestige image in background of the top

--- src/components/Card/CardPie.js
+++ src/components/Card/CardPie.js
@@ -1,31 +1,36 @@
 import React from "react";
 import './Card.css'
-import {VictoryPie} from 'victory-pie';
-
+import {Doughnut} from 'react-chartjs-2';
 export const CardPie = ({name, data1, data2, sub}) => {
     const orange = '#b27b00';
     const blackOrange = '#6e4c00';
+    const data = {
+        labels: [
+            `${data1}%`,
+            `${data2}%`
+        ],
+        datasets: [{
+            data: [data1, data2],
+            backgroundColor: [
+                '#ff9202',
+                '#694603',
+            ],
+            hoverBackgroundColor: [
+                '#ff9202',
+                '#694603',
+            ]
+        }]
+    };
     return (<div className={'card'}>
         <div className={'card-container-pie'}>
             <div className={'card-top'}>{name}</div>
-            <div className={'card-context-pie'}><VictoryPie
-                colorScale={[orange, blackOrange]}
-                data={[
-                    {x: `${data1}%`, y: data1},
-                    {x: `${data2}%`, y: data2},
-                ]}
-                padAngle={3}
-                innerRadius={100}
-                labelRadius={110}
-                style={{
-                    labels: {
-                        fill: "black",
-                        fontSize: 20,
-                        fontWeight: "bold",
-                        fontFamily: 'Titillium Web, sans-serif'
-                    }
-                }}
-            /></div>
+
+            <Doughnut data={data}
+                      width={150}
+                      height={100}
+                      options={{
+                          maintainAspectRatio: false
+                      }}/>
             <div className={'card-sub'}>{sub} </div>
         </div>
     </div>)
